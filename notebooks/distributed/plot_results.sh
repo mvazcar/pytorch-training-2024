7aa1e13e29a2afc83c7176e56d2453fedf2958d8
boeschf <48126478+boeschf@users.noreply.github.com>
2025-07-02 01:39:43 +0200

    better mount for container

--- notebooks/distributed/plot_results.sh
+++ notebooks/distributed/plot_results.sh
@@ -3,6 +3,6 @@
 ENROOT_SLURM_HOOK=0 enroot start \
     --mount /capstor:/capstor \
     --mount /iopsstor:/iopsstor \
-    --mount /iopsstor/scratch/cscs/boeschf/pytorch-training/notebooks/distributed:/workspace \
+    --mount "$(pwd)":/workspace \
     /capstor/store/cscs/cscs/jupyter/pytorch/pytorch-course-tf-25.05-py3.sqsh \
-    bash -c "python collect_results.py; python plot_results.py"
+    bash -c "python collect_results.py && python plot_results.py"
