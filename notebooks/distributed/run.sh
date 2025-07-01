5e11c3a99538ef06f90e2e7b6ea81bda66e949a8
boeschf <48126478+boeschf@users.noreply.github.com>
2025-07-01 22:50:42 +0200

    master address

--- notebooks/distributed/run.sh
+++ notebooks/distributed/run.sh
@@ -17,20 +17,14 @@
 #SBATCH --output=/iopsstor/scratch/cscs/boeschf/pytorch-training/notebooks/distributed/train.log
 ##SBATCH --error=/iopsstor/scratch/cscs/${USER}/pytorch-training/notebooks/distributed/train.err
 
-# Get master node.
-# Get IP for hostname.
+export DISTRIBUTED_INITIALIZATION_METHOD=SLURM
 MASTER_ADDR="$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)"
 MASTER_ADDR="$(getent ahosts "$MASTER_ADDR" | awk '{ print $1; exit }')"
-
-# Choose method to initialize dist in pytorch
-export DISTRIBUTED_INITIALIZATION_METHOD=SLURM
 export MASTER_ADDR
 export MASTER_PORT=29500
 export WORLD_SIZE=$SLURM_NPROCS
 export LOCAL_RANK=$SLURM_LOCALID
-
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1 
-#export TRITON_HOME=/dev/shm/
 export MPICH_GPU_SUPPORT_ENABLED=0
 export CUDA_CACHE_DISABLE=1
 export OMP_NUM_THREADS=64
